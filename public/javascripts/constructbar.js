5ac51d488a10609de7de8271d5dad9ac60136212
a4a881d4 <a4a881d4@163.com>
2012-08-18 01:33:02 +0800

    some bugs

--- public/javascripts/constructbar.js
+++ public/javascripts/constructbar.js
@@ -2,6 +2,12 @@ $(function() {
   $.get("/menu","",function(data,textStatus) {
     if( textStatus == 'success' ) {
       var menus=JSON.parse(data);
+      var V = {};
+      V.id = 'new';
+      V.name = '新建';
+      var astr = {'admin':'','operator':'','watch':'','guest':''};
+      V.auth = astr;
+      menus.push(V);
       var str = '<ul class="nav nav-tabs"/>';
       $("#side").append(str);
       for( var menu in menus ) {  
@@ -13,18 +19,24 @@ $(function() {
       for( var menu in menus ) {  
         str = '<div id="'+menus[menu].id+'" class="tab-pane"/>';
         $("#side .tab-content").append(str);
-        str = '<input id="'+menus[menu].id+'" name="name" class="span2"/>';
-        $('#side .tab-content #'+menus[menu].id).append(str);
-        $('#side .tab-content input#'+menus[menu].id).attr('value',menus[menu].name);
+        str = '<form method="post" action="/menu" id="form_'+menus[menu].id+'"/>';
+        $("#side .tab-content .tab-pane#"+menus[menu].id).append(str);
+        if( menus[menu].id == 'new' ) {
+          str = '<lable>id:</lable><input id="id_'+menus[menu].id+'" name="id" class="span2"/>';
+          $('#side .tab-content .tab-pane #form_'+menus[menu].id).append(str);
+        }
+        str = '<input id="name_'+menus[menu].id+'" name="name" class="span2"/>';
+        $('#side .tab-content .tab-pane #form_'+menus[menu].id).append(str);
+        $('#side .tab-content .tab-pane input#name_'+menus[menu].id).attr('value',menus[menu].name);
         var auth = menus[menu].auth;
         for( var ath in auth ) {
           str = authButton(ath,auth[ath],menus[menu].id);
-          $('#side .tab-content .tab-pane#'+menus[menu].id).append(str);
+          $('#side .tab-content .tab-pane #form_'+menus[menu].id).append(str);
         }
-        str = '<hr/><button id="submit_'+menus[menu].id+'" class="btn btn-primary">修改</button>';
-        $('#side .tab-content .tab-pane#'+menus[menu].id).append(str);
-        str = '<button id="delete_'+menus[menu].id+'" class="btn btn-danger">删除</button>';
-        $('#side .tab-content .tab-pane#'+menus[menu].id).append(str);
+        str = '<hr/><button id="submit_'+menus[menu].id+'" class="btn btn-primary"><i class="icon-refresh icon-white"/>修改</button>';
+        $('#side .tab-content .tab-pane #form_'+menus[menu].id).append(str);
+        str = '<button id="delete_'+menus[menu].id+'" class="btn btn-danger"><i class="icon-remove icon-white"/>删除</button>';
+        $('#side .tab-content .tab-pane #form_'+menus[menu].id).append(str);
       }
       $('#side .nav.nav-tabs a:first').tab('show');
       $('a[data-toggle="tab"]').on('shown',function(e) {
@@ -54,15 +66,17 @@ $(function() {
                   str = authButton(ath,auth[ath],menus[menu].id);
                   $('#main .tab-content .tab-pane#'+menus[menu].id).append(str);
                 }
-                str = '<hr/><button id="submit_'+menus[menu].id+'" class="btn btn-primary">修改</button>';
+                str = '<hr/><button id="submit_'+menus[menu].id+'" class="btn btn-primary"><i class="icon-refresh icon-white"/>修改</button>';
                 $('#main .tab-content .tab-pane#'+menus[menu].id).append(str);
-                str = '<button id="delete_'+menus[menu].id+'" class="btn btn-danger">删除</button>';
+                str = '<button id="delete_'+menus[menu].id+'" class="btn btn-danger"><i class="icon-remove icon-white"/>删除</button>';
                 $('#main .tab-content .tab-pane#'+menus[menu].id).append(str);
               } else {
-                str = '<input type="text"/>';
+              	str = '<lable>Tilte:</lable><input class="span4" value="'+menus[menu].title+'"/>';
+              	$('#main .tab-content .tab-pane#'+menus[menu].id).append(str);
+                str = '<textarea border="1" class="field span8" rows="6"/>';
                 $('#main .tab-content .tab-pane#'+menus[menu].id).append(str);
-                $('#main .tab-content .tab-pane#'+menus[menu].id+' input').attr('value',menus[menu].content);
-                str = '<hr/><button id="submit_'+menus[menu].id+'" class="btn btn-primary">修改</button>';
+                $('#main .tab-content .tab-pane#'+menus[menu].id+' textarea').attr('value',menus[menu].content);
+                str = '<hr/><button id="submit_'+menus[menu].id+'" class="btn btn-primary"><i class="icon-refresh icon-white"/>修改</button>';
                 $('#main .tab-content .tab-pane#'+menus[menu].id).append(str);
               }
             }
@@ -76,7 +90,7 @@ $(function() {
 
 function authButton( ath, right, id ) {
   var str = '<label> '+ath+'</label>';
-  str += '<div class="btn-group" data-toggle="buttons-checkbox" id="'+id+'" name="'+ath+'">';
+  str += '<div class="btn-group" data-toggle="buttons-checkbox" id="_'+id+'" name="'+ath+'">';
   var rs = { 'a':'A', 'd':'D', 'r':'R', 'w':'W', 'x':'X' };
   for( var r in rs ) {
     if( right.indexOf(r) == -1 ) {
